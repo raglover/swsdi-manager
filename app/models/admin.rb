e08cd4d1847e6e97d021f460b133249374387d5c
Richard Glover <raglover@gmail.com>
2014-05-04 05:59:35 -0700

    Undo second Omniauthable model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,6 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :omniauthable, :confirmable, 
-         :recoverable, :rememberable, :trackable, :validatable, :omniauth_providers => [ :facebook ]
+  devise :database_authenticatable, :registerable, :confirmable, 
+         :recoverable, :rememberable, :trackable, :validatable
 end
