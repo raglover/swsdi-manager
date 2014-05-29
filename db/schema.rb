29d561458cda48e542cacc4a70f643ee9cb04eea
Richard Glover <richard@gloverdesigns.com>
2014-05-29 00:02:07 -0700

    Add some functionality to Devise Admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525044958) do
+ActiveRecord::Schema.define(version: 20140528101210) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,10 +26,19 @@ ActiveRecord::Schema.define(version: 20140525044958) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
 
+  add_index "admins", ["confirmation_token"], name: "index_admins_on_confirmation_token", unique: true
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  add_index "admins", ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
 
   create_table "camp_applications", force: true do |t|
     t.integer  "user_id"
