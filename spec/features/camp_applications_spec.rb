9937990a8b7bc10dabf651544bd8b720b70d543b
Richard Glover <richard@gloverdesigns.com>
2014-12-30 19:18:35 -0700

    Fixed some error messages in my specs
    Had some failing specs. Fixed them. All good!
    
    Signed-off-by: Richard Glover <richard@gloverdesigns.com>

--- spec/features/camp_applications_spec.rb
+++ spec/features/camp_applications_spec.rb
@@ -3,12 +3,13 @@ require 'requests_helper'
 
 describe "CampApplicationPage", type: :feature do
   let(:user) { FactoryGirl.create(:complete_user) }
-  before(:all) { FactoryGirl.create(:camp) }
+  let(:camp) { FactoryGirl.create(:camp) }
 
   context "GET /camp_application/id" do
-    let(:app) { FactoryGirl.create(:camp_application) }
     
     it "Shows the user's application information" do
+      login_as(user, scope: :user)
+      app = FactoryGirl.create(:camp_application, user: user, camp: camp)
       visit(camp_application_path(app))
       expect(page).to have_content app.camper_type
     end
