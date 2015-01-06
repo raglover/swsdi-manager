2746b9fe511939f14fd0fef848fa955fabe20318
Richard Glover <richard@gloverdesigns.com>
2015-01-06 07:37:17 -0700

    Updated some stuff to match what's in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,16 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'swsdi-manager.dev' }
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'swsdi.org' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { host: 'smtp.mandrillapp.com',
+                                         port: '587',
+                                         user_name: Rails.application.secrets.mandrill_username,
+                                         password: Rails.application.secrets.mandrill_api_key,
+                                         authentication: :plain,
+                                         domain: 'swsdi.org',}
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
