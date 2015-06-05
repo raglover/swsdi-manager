cac97a40a662f572bdc71ddecdcf1ef3de9147eb
Richard Glover <richard@gloverdesigns.com>
2015-06-05 00:57:46 -0700

    Added ability for admins to edit user records
    Signed-off-by: Richard Glover <richard@gloverdesigns.com>

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-
+  before_filter :authenticate_admin!, only: [:admin_edit, :admin_update_user]
 
   def update
     @user = User.find(current_user.id)
@@ -20,6 +20,19 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  def admin_edit
+    @user = User.find(params[:id])
+  end
+
+  def admin_update_user
+    @user = User.find(params[:id])
+    if @user.update_without_password(devise_parameter_sanitizer.sanitize(:admin_account_update))
+      set_flash_message :notice, :updated
+      redirect_to profiles_path
+    else
+      render "admin_edit"
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,24 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-  get 'reports/financial'
-  end
-
-  namespace :admin do
-  get 'reports/shuttle'
-  end
-
-  namespace :admin do
-  get 'reports/status'
-  end
-
-  root 'info#index'
 
   devise_for :admins, controllers: { sessions: "admin/sessions", registrations: "admin/registrations", 
     confirmations: "admin/confirmations", passwords: "admin/passwords" }
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks", registrations: "registrations"}
+
+  devise_scope :user do
+    get 'admin-edit/:id', controller: 'registrations', action: 'admin_edit', as: 'admin_user_edit'
+    put 'admin-edit/:id', controller: 'registrations', action: 'admin_update_user'
+  end
 
   namespace :admin do
     get 'dashboard/index'
     get 'dashboard/event_facebook/:id', to: 'dashboard#event_facebook'
     get 'admins/index'
     get 'admins/show'
+    get 'reports/status'
+    get 'reports/shuttle'
+    get 'reports/financial'
+
     resources :camps
     resources :camp_applications, only: [:index, :show, :edit, :update, :destroy] do
       resources :payments, except: [:show]
@@ -38,5 +34,6 @@ Rails.application.routes.draw do
   get 'info/license'
   get 'profiles/send_reminder_email/:id', to: 'profiles#send_reminder_email'
 
+  root 'info#index'
 
 end
