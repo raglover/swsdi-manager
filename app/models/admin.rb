0ce4f02436720fa8606d6b3fd640e7802f863fe6
Richard Glover <richard@gloverdesigns.com>
2015-07-02 14:56:39 -0700

    Remove registration for admins
    Signed-off-by: Richard Glover <richard@gloverdesigns.com>

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,7 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, :lockable,
+  devise :database_authenticatable, :confirmable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :admin_profile
