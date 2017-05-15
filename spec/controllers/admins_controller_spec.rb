35133ca6109be5bddf25a6444919c4bd14564258
Richard Glover <richard@gloverdesigns.com>
2017-05-14 17:41:26 -0700

    Some updates to specs

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe AdminsController, type: :controller do
+describe Admin::AdminsController, type: :controller do
 
   describe "GET 'index'" do
     it "returns http success" do
