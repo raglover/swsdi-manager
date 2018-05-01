b345ad00f34099d0d34ce7dc47c9917c8d566a4b
Richard Glover <richard@gloverdesigns.com>
2018-04-30 18:13:59 -0700

    Attempted fix to the view

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -10,7 +10,11 @@ class Admin < ActiveRecord::Base
   scope :counselors, -> {where("role = 'counselor' OR role = 'slc'")}
 
   def full_name
-    "#{self.admin_profile.first_name.capitalize} #{self.admin_profile.last_name.capitalize}"
+    if self.admin_profile
+      "#{self.admin_profile.first_name.capitalize} #{self.admin_profile.last_name.capitalize}"
+    else
+      "Name Goes here..."
+    end
   end
 
   def super_admin?
