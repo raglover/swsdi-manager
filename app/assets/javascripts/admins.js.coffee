483f23318b3634de4374c7eafa77871a2b49cc0f
Richard Glover <richard@gloverdesigns.com>
2023-02-18 18:26:28 -0500

    Add ability to edit admin roles
    Added the ability for a superuser to edit the roles of administrators (So I don’t have to fire up a console on prod to do the task when I need to.)
    
    Next, I’d like to make it ajax-y, but I don’t have the patience at the moment for JS debugging.
    
    Signed-off-by: Richard Glover <richard@gloverdesigns.com>

--- app/assets/javascripts/admins.js.coffee
+++ app/assets/javascripts/admins.js.coffee
@@ -1,3 +1,21 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+# # Place all the behaviors and hooks related to the matching controller here.
+# # All this logic will automatically be available in application.js.
+# # You can use CoffeeScript in this file: http://coffeescript.org/
+
+# $ ->
+#   $('select#admin_role').on 'change', ->
+#     form = $(this).closest('form')
+#     url = form.attr('action')
+#     data = form.serialize()
+#     console.log form
+#     console.log url
+#     console.log data
+#     $.ajax
+#       url: url
+#       type: "PATCH"
+#       dataType: "json"
+#       data: data
+#       success: (data) ->
+#         console.log "Administrator Role updated successfully"
+#       error: (jqXHR, textStatus, errorThrown) ->
+#         console.log "Error updating admin"
--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,5 +1,6 @@
 class Admin::AdminsController < ApplicationController
   before_filter :set_camp
+  before_action :authenticate_admin!
 
   def index
     @admins = Admin.all
@@ -9,9 +10,31 @@ class Admin::AdminsController < ApplicationController
     @admins = Admin.all
   end
 
+  def edit
+  end
+
+  def update
+    @admin = Admin.find(params[:id])
+    respond_to do |format|
+      if @admin.update(admin_params)
+        format.html { redirect_to admin_admins_path, notice: "Admin Role successfully updated!" }
+        format.json { render :index, status: :ok, location: admin_admins_path }
+      else
+        format.html { render :edit, alert: "There was a problem updating the record." }
+        format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def destroy
     @admin = Admin.find_by_id(params[:id])
     @admin.destroy!
     redirect_to admin_admins_path, notice: 'Staff Record was successfully destroyed.'
   end
+
+  private
+
+    def admin_params
+      params.require(:admin).permit(:role)
+    end
 end
