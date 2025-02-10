65134a166fb96dd35fd73e6db2cb4cdf3003e9ea
Richard Glover <richard@gloverdesigns.com>
2025-02-10 16:45:40 -0700

    Updates for new year

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,56 +1,55 @@
 class RegistrationMailer < ActionMailer::Base
-	default from: 'info@swsdi.org'
+  default from: 'info@swsdi.org'
 
-	def student_email(user, camp)
-		@user = user
-		@camp = camp
-		@pay_url = 'https://na.eventscloud.com/swsd2022'
-		@url = 'https://register.swsdi.org/documents/returnpacket24.pdf'
-    @docurl = 'https://register.swsdi.org/documents/infopacket24.pdf'
-		mail(to: @user.email, 
-			subject: "Thank you for registering for #{@camp.name} - Next Steps")
-	end
+  def student_email(user, camp)
+    @user = user
+    @camp = camp
+    @pay_url = 'https://na.eventscloud.com/swsd2022'
+    @url = 'https://register.swsdi.org/documents/returnpacket25.pdf'
+    @docurl = 'https://register.swsdi.org/documents/infopacket25.pdf'
+    mail(to: @user.email,
+         subject: "Thank you for registering for #{@camp.name} - Next Steps")
+  end
 
-	def parent_email(user, camp)
-		@user = user
-		@camp = camp
-		@pay_url = 'https://na.eventscloud.com/swsd2022'
-		@url = 'https://register.swsdi.org/documents/returnpacket24.pdf'
-    @docurl = 'https://register.swsdi.org/documents/infopacket24.pdf'
-		mail(to: @user.parent_email, 
-			subject: "Your Student #{@user.first_name} has registered for #{@camp.name} - Next Steps")
-	end
+  def parent_email(user, camp)
+    @user = user
+    @camp = camp
+    @pay_url = 'https://na.eventscloud.com/swsd2022'
+    @url = 'https://register.swsdi.org/documents/returnpacket25.pdf'
+    @docurl = 'https://register.swsdi.org/documents/infopacket25.pdf'
+    mail(to: @user.parent_email,
+         subject: "Your Student #{@user.first_name} has registered for #{@camp.name} - Next Steps")
+  end
 
-	def coach_email(user, camp, app)
-		@user = user
-		@camp = camp
-		@id = app.uuid
-		@url = "https://register.swsdi.org/coach_comments/new/?id=#{@id}"
-		mail(to: @user.coach_email, 
-			subject: "Your Student #{@user.first_name} has registered for #{@camp.name} - Next Steps")
-	end
+  def coach_email(user, camp, app)
+    @user = user
+    @camp = camp
+    @id = app.uuid
+    @url = "https://register.swsdi.org/coach_comments/new/?id=#{@id}"
+    mail(to: @user.coach_email,
+         subject: "Your Student #{@user.first_name} has registered for #{@camp.name} - Next Steps")
+  end
 
-	def admin_email(user, camp, app)
-		@user = user
-		@camp = camp
-        @app = app
-        mail(to: "Richie Glover <richard@swsdi.org>, Adam Symonds <adam@swsdi.org>", 
-			subject: "New Application submitted for #{@camp.name} - #{@user.first_name} #{@user.last_name}")
-	end
+  def admin_email(user, camp, app)
+    @user = user
+    @camp = camp
+    @app = app
+    mail(to: 'Richie Glover <richard@swsdi.org>, Adam Symonds <adam@swsdi.org>',
+         subject: "New Application submitted for #{@camp.name} - #{@user.first_name} #{@user.last_name}")
+  end
 
-	def verification_email(user, camp, camp_app)
-		@user = user
-		@camp = camp
-		@camp_app = camp_app
-		mail(to: [@user.email, @user.parent_email],
-			subject: "#{@user.first_name}'s #{@camp.name} Registration has been verified!")
-	end
+  def verification_email(user, camp, camp_app)
+    @user = user
+    @camp = camp
+    @camp_app = camp_app
+    mail(to: [@user.email, @user.parent_email],
+         subject: "#{@user.first_name}'s #{@camp.name} Registration has been verified!")
+  end
 
-	def reminder_email(user, camp)
-		@user = user
-		@camp = camp
-		mail(to: @user.email,
-			subject: "#{@user.first_name}, Please finish registering for SWSD!")
-	end
-
+  def reminder_email(user, camp)
+    @user = user
+    @camp = camp
+    mail(to: @user.email,
+         subject: "#{@user.first_name}, Please finish registering for SWSD!")
+  end
 end
--- app/services/calculate_tuition.rb
+++ app/services/calculate_tuition.rb
@@ -1,112 +1,96 @@
 class CalculateTuition
-
   def initialize(camp_app)
     @camp_app = camp_app
-    #TODO: Make these editable/settable at the Camp level.
+    # TODO: Make these editable/settable at the Camp level.
     @max_res_tuition = 1995
     @max_com_tuition = 1550
-    @app_fee = 100
+    @app_fee = 200
     @breakfast_fee = 150
   end
 
   def financial_aid
-    finaid = @camp_app.payments.where(pmt_type: ["Financial Aid", "Scholarship"]).pluck(:amount)
-    finaid.reduce(0){|sum, val| sum + val}
+    finaid = @camp_app.payments.where(pmt_type: ['Financial Aid', 'Scholarship']).pluck(:amount)
+    finaid.reduce(0) { |sum, val| sum + val }
   end
 
   def max_scholarship_limit_allowed(new_scholarship)
-    scholarship_amt = @camp_app.payments.where(pmt_type: ["Scholarship"]).pluck(:amount)
-    scholarship_total = (scholarship_amt.reduce(0){|sum, val| sum + val} + new_scholarship)
-    if @camp_app.camper_type == "Resident"
-      if scholarship_total <= (@max_res_tuition * 0.6)
-        return true
-      else 
-        return false
-      end
-    elsif @camp_app.camper_type == "Commuter"
-      if scholarship_total <= (@max_com_tuition * 0.6)
-        return true
-      else
-        return false
-      end
+    scholarship_amt = @camp_app.payments.where(pmt_type: ['Scholarship']).pluck(:amount)
+    scholarship_total = (scholarship_amt.reduce(0) { |sum, val| sum + val } + new_scholarship)
+    if @camp_app.camper_type == 'Resident'
+      return true if scholarship_total <= (@max_res_tuition * 0.6)
+
+      false
+
+    elsif @camp_app.camper_type == 'Commuter'
+      return true if scholarship_total <= (@max_com_tuition * 0.6)
+
+      false
+
     else
-      return false
+      false
     end
   end
 
   def total_paid
-    paid = @camp_app.payments.where.not(pmt_type: ["Financial Aid", "Scholarship"]).pluck(:amount)
-    if @camp_app.app_fee
-      paid << @app_fee
-    end
-    paid.reduce(0){|sum, val| sum + val}
+    paid = @camp_app.payments.where.not(pmt_type: ['Financial Aid', 'Scholarship']).pluck(:amount)
+    paid << @app_fee if @camp_app.app_fee
+    paid.reduce(0) { |sum, val| sum + val }
   end
 
   def base_tuition
-    if @camp_app.camper_type == "Resident"
-      return res_base_cost
-    elsif @camp_app.camper_type == "Commuter"
-      return com_base_cost
+    if @camp_app.camper_type == 'Resident'
+      res_base_cost
+    elsif @camp_app.camper_type == 'Commuter'
+      com_base_cost
     end
-  end   
+  end
 
   def total_owed
-    if @camp_app.camper_type == "Resident"
-      return (res_base_cost)
-    elsif @camp_app.camper_type == "Commuter"
-      return (com_base_cost)
+    if @camp_app.camper_type == 'Resident'
+      res_base_cost
+    elsif @camp_app.camper_type == 'Commuter'
+      com_base_cost
     end
   end
 
   def current_balance
     starting_balance = subtract_app_fee(total_owed)
-    balance = subtract_payments(starting_balance)
-    return balance
+    subtract_payments(starting_balance)
   end
 
   private
 
-    def res_base_cost
-      eventfees = @camp_app.events.pluck(:price_resident)
-      event_cost = eventfees.reduce(0){|sum, value| sum + value}
-      if event_cost > @max_res_tuition
-        return @max_res_tuition + fees_owed
-      else
-        return event_cost + fees_owed
-      end
-    end
+  def res_base_cost
+    eventfees = @camp_app.events.pluck(:price_resident)
+    event_cost = eventfees.reduce(0) { |sum, value| sum + value }
+    return @max_res_tuition + fees_owed if event_cost > @max_res_tuition
 
-    def com_base_cost
-      eventfees = @camp_app.events.pluck(:price_commuter)
-      event_cost = eventfees.reduce(0){|sum, value| sum + value}
-      if event_cost > @max_com_tuition
-        return @max_com_tuition + fees_owed
-      else
-        return event_cost + fees_owed
-      end
-    end
+    event_cost + fees_owed
+  end
 
-    def fees_owed
-      fees = 0
-      if @camp_app.breakfast
-        fees += @breakfast_fee
-      end
-      return fees
-    end
+  def com_base_cost
+    eventfees = @camp_app.events.pluck(:price_commuter)
+    event_cost = eventfees.reduce(0) { |sum, value| sum + value }
+    return @max_com_tuition + fees_owed if event_cost > @max_com_tuition
 
-    def subtract_app_fee(total)
-      if @camp_app.app_fee
-        return total ? total - @app_fee : 0
-      else
-        return total
-      end
-    end
+    event_cost + fees_owed
+  end
 
-    def subtract_payments(balance)
-      payments = @camp_app.payments.pluck(:amount)
-      total_pmt = payments.reduce(0.0){|sum, value| sum + value}
-      balance =  balance && balance - total_pmt >= 0 ? balance - total_pmt : 0
-      return balance
-    end
+  def fees_owed
+    fees = 0
+    fees += @breakfast_fee if @camp_app.breakfast
+    fees
+  end
 
+  def subtract_app_fee(total)
+    return total ? total - @app_fee : 0 if @camp_app.app_fee
+
+    total
+  end
+
+  def subtract_payments(balance)
+    payments = @camp_app.payments.pluck(:amount)
+    total_pmt = payments.reduce(0.0) { |sum, value| sum + value }
+    balance && balance - total_pmt >= 0 ? balance - total_pmt : 0
+  end
 end
